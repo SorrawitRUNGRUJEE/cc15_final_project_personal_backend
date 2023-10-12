ea9ee2e145df6c1ef3962f0ec12399ec589c33c2
SorrawitRUNGRUJEE <sorrawit_r@outlook.com>
2023-10-12 14:52:06 +0700

    adjust the following schema => add the super admin controller => added the logic for login, tested complted note added log in credential info

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+const cors = require('cors')
+const morgan = require('morgan')
 const port  = process.env.PORT
 const auth_route = require('./src/routes/auth_route')
 const hdl_error = require('./src/middlewares/hdl_error')
 const hdl_resource_not_found = require('./src/middlewares/hdl_resource_not_found')
 app.use(express.json())
-
+app.use(cors())
+app.use(morgan('short'))
 
 app.use('/auth',auth_route)
 app.use(hdl_error)
--- src/controller/auth_controller.js
+++ src/controller/auth_controller.js
@@ -1,21 +1,20 @@
 const createError = require("../utils/create_error");
-const { registerSchema,loginSchema } = require("../validation/auth_validator");
+const { registerSchema, loginSchema } = require("../validation/auth_validator");
 const bcrypt = require("bcryptjs");
 const prisma = require("../model/prisma");
-const Jwt = require('jsonwebtoken')
+const Jwt = require("jsonwebtoken");
 
 exports.register = async (req, res, next) => {
   try {
     const { value, error } = registerSchema.validate(req.body);
-    
-    if (error) return next(createError(error, 400));
+
+    if (error) return next(error);
     const existEmail = await prisma.user.findUnique({
       where: {
         email: value.email,
       },
     });
-    if (existEmail)
-      return res.status(400).json({ msg: "email already in used" });
+    if (existEmail) return res.status(400).json({ msg: "email already in used" });
     if (error) return next(createError(error, 400));
     const existUsername = await prisma.user.findUnique({
       where: {
@@ -26,42 +25,38 @@ exports.register = async (req, res, next) => {
     if (existUsername)
       return res.status(400).json({ msg: "username already in used" });
     value.password = await bcrypt.hash(value.password, 12);
-    const user = await prisma.user.create({
+    await prisma.user.create({
       data: value,
     });
-  
 
-
-
-
-    res.status(200).json({msg:"registration success"});
+    res.status(200).json({ msg: "registration success" });
   } catch (err) {
     next(err);
   }
 };
 
-exports.login = async (req,res,next) =>{
-  try{
-    const {value,error} = loginSchema.validate(req.body)
-    console.log('here',value)
-    if (error) return next(createError(error, 400))
+exports.login = async (req, res, next) => {
+  try {
+    const { value, error } = loginSchema.validate(req.body);
+    console.log("here", value);
+    if (error) return next(createError(error, 400));
     const user = await prisma.user.findFirst({
-        where:{
-            username:value.username
-        }
-    })
-    if(!user) return next(createError("invalid credential", 400))
-    const isMatch  = await bcrypt.compare(value.password,user.password)
-    if(!isMatch) return next(createError("invalid credentials", 400))
-    const payload = user.id
-    const accessToken = Jwt.sign({payload},process.env.JWT_SECRET_KEY ||"kjasbdf",{expiresIn:process.env.JWT_EXPIRES})   
-    res.status(200).json({msg:"access granted",accessToken,user})
-
-}catch(err){
-
-next(createError(err,500))
-}
-
-
-
-}
+      where: {
+        username: value.username,
+      },
+    });
+    if (!user) return next(createError("invalid credential", 400));
+    const isMatch = await bcrypt.compare(value.password, user.password);
+    if (!isMatch) return next(createError("invalid credentials", 400));
+    const payload = user.id;
+    const accessToken = Jwt.sign(
+      { payload },
+      process.env.JWT_SECRET_KEY || "kjasbdf",
+      { expiresIn: process.env.JWT_EXPIRES }
+    );
+    delete user.password
+    res.status(200).json({ msg: "access granted", accessToken, user });
+  } catch (err) {
+    next(createError(err, 500));
+  }
+};
